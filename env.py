163ac8992e94e68cda02dd7a615ca427b191a881
Harish kanchi <harishkanchi.rishi@gmail.com>
2024-04-27 13:11:26 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -2,3 +2,6 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://mlluuzqh:vYsx9WHaFGXQujdaqz_aGyQSDgE3A_l2@flora.db.elephantsql.com/mlluuzqh")
+os.environ.setdefault(
+    "CLOUDINARY_URL","cloudinary://184944431334241:ciX4KKehikINrTZtHML9G3qX7xA@dftcpdww9"
+    )
